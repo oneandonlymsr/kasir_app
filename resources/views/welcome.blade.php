a3766d09ba622fe23d1699f2c9dafb88cef0e9ba
Mala Sani Rakhma <malasani30@gmail.com>
2026-05-15 12:10:24 +0700

    Selesai setting layout Roawring Coffee

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+
+@section('content')
+<div style="text-align: center; width: 90%; max-width: 700px; margin:auto; background: rgba(0, 0, 0, 0, 0.85); padding: 30px; border-radius: 15px; border: 3px solid #ff8c00; box-shadow: 0 0 10px #ff8c00;">
+   <h1 style="font-size: 2rem; font-weight: bold; color:#ff8c00; margin-bottom: 10px; ">Selamat Datang di The Roawring Coffee!</h1>
+   <p style="font-size: 1.2rem; color: #706f6c; margin-bottom: 20px;">Sistem Kasir Ini Sudah Berhasil Menggunakan Master Layout</p>
+   <button style="background: #ff8c00; color: black; padding: 10px 25px; border:#706f6c; border-radius: 5px; font-weight: bold; cursor: ponter; text-transform:uppercase;">
+      Mulai Transaksi
+   </button>
+  </div>
+@endsection
